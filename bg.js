3144ddacdc1f49b744ec7f99ba610b4fd10b10f2
ertanyildiz <ertanyildizz@gmail.com>
2017-03-06 21:58:58 +0200

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,47 +1,38 @@
 $( document ).ready(function() {
 
-	for (var i = 0; i < localStorage.length; i++){
-		console.log(localStorage.getItem(localStorage.key(i)));
-    //$('#liste').append("<li class='list-group-item list-group-item-danger' style='width: 420px;>"+localStorage.getItem(localStorage.key(i))+" <span class='badge'>X</span></li>");
-    $('#liste').append("<li class='list-group-item list-group-item-danger' style='width: 420px;''>"+localStorage.getItem(localStorage.key(i))+" <span class='badge'><a id="+localStorage.key(i)+" class='silBtn'>X</a></span></li>");
-    
+
+for (var i = 0; i < localStorage.length; i++){
+    $('#liste').append("<li class='list-group-item list-group-item-danger' style='width: 420px;''>"+localStorage.getItem(localStorage.key(i))+" <span class='badge'><a id="+localStorage.key(i)+" class='silBtn'>Sil X</a></span></li>"); 
 }
 
 $('#h1Liste').append(" ("+ localStorage.length +")");
 	
-
-
- 	$( "#eklebtn" ).click(function() {
-
+ $( "#eklebtn" ).click(function() {
 		var engelli = $("#engelli").val();
  		if (localStorage.getItem(engelli) != null) {
  			alert("Eklenmiş!");
  			location.reload();
  			return;
- 		}
+}
 
 
-
- 	localStorage.setItem(engelli, engelli);
- 	
-
- 	chrome.storage.sync.set({engelli:engelli});
-
-    location.reload();
-
+localStorage.setItem(engelli, engelli);
+chrome.storage.local.set({[engelli]:[engelli]});
+location.reload();
 });
 
 $(".silBtn").click(function() {
 	var item = this.id;
   	localStorage.removeItem(item);
-  	chrome.storage.sync.get(item, function() {});
+  	chrome.storage.local.remove(item, function() {});
   	location.reload();
 });
 
 $("#silHepsiBtn").click(function() {
   	if (confirm('Hepsini sil?')) {
- 			localStorage.clear();
- 			location.reload();
+ 	    localStorage.clear();
+ 	    chrome.storage.local.clear();
+ 	    location.reload();
 } 
 
 });
